35c584a2cee0b85af11a12b1cd0e3bce73af5405
Chpsterz <paaxyman@gmail.com>
2022-05-30 14:19:19 -0400

    Fixed localstorage

--- js/update/textUpdate.js
+++ js/update/textUpdate.js
@@ -9,5 +9,10 @@ function updateText(refer) {
     refer.convertWaterToFuel.setText(refer.getData.water + ' Water Molecule = ' + refer.getData.water * 2 + ' Hydrogen and ' + refer.getData.water + ' Oxygen');
     refer.convertHydrogenToFuel.setText(refer.getData.hydrogen + ' Hydrogen = ' + refer.getData.hydrogen + ' Fuel');
     refer.waterText.setText('Water: ' + refer.getData.water);
-    localStorage.setItem("gameData", JSON.stringify({'oxygen': refer.getData.oxygen, 'hydrogen': refer.getData.hydrogen, 'water': refer.getData.water, 'fuel': refer.getData.fuel, 'counter': refer.getData.counter}))
+    if(localStorage.getItem("gameData").fuel !== 2000) {
+        localStorage.setItem("gameData", JSON.stringify({'oxygen': refer.getData.oxygen, 'hydrogen': refer.getData.hydrogen, 'water': refer.getData.water, 'fuel': refer.getData.fuel, 'counter': refer.getData.counter}))
+    }
+    else if(localStorage.getItem("gameData").fuel === 2000) {
+        localStorage.setItem("gameData", JSON.stringify({'oxygen': 0, 'hydrogen': 0, 'water': 0, 'fuel': 2000, 'counter': 0}))
+    }
 }
