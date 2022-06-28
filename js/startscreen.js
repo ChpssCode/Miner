58b2f7fcbdb556bd013f3a866dcd62a4a7aab49d
Chpsterz <paaxyman@gmail.com>
2022-06-28 17:53:39 -0400

    Added Startscreen Music

--- js/startscreen.js
+++ js/startscreen.js
@@ -10,7 +10,8 @@ class Startscreen extends Phaser.Scene
     preload() {
     this.load.image("background", "assets/background.png")
     this.load.image("logo", "assets/logo.png");
-    this.load.image("play", "assets/play.png")
+    this.load.image("play", "assets/play.png");
+    this.load.audio('theme', 'assets/bg_music.ogg');
     }
 
     create() {
@@ -18,9 +19,13 @@ class Startscreen extends Phaser.Scene
     this.add.text(window.innerWidth / 7, window.innerHeight / 3, "Miner is a 2D Idle Game\nwere you goal is to farm planets\nresources in exchange for in-game\ncurrency. the more resources you\nget, the farther you can expand \nyour economic emprire! ", { fontFamily: "'Open Sans', sans-serif, 'Roboto', sans-serif, 'Roboto Mono', monospace"}).setScrollFactor(0).setFontSize(18).setColor('#ffffff').setBackgroundColor("#000000");
     this.logo = this.physics.add.image(window.innerWidth / 2, window.innerHeight / 2.5, "logo").setScale(1.4).setDepth(3);                                       
     this.play = this.physics.add.image(window.innerWidth / 2, window.innerHeight / 1.7, "play").setScale(1);
-
+    this.theme = this.sound.add('theme');
+    this.theme.play()
     this.play.setInteractive();
-    this.play.on("pointerup", () => this.scene.start('Game') )
+    this.play.on("pointerup", () => { 
+        this.scene.start('Game') 
+        this.theme.stop();
+      } )
     }
     update() {
         this.logo.setPosition(window.innerWidth / 2, window.innerHeight / 2.5)
